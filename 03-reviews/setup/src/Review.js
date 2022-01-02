613f9b09bf5649cdf9c287ec4b7bd4031cc48532
Anita Lipsky <anita@purplebugs.com>
2022-01-02 08:34:04 +0100

    setIndex to random

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -32,6 +32,14 @@ const Review = () => {
     });
   };
 
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -51,7 +59,9 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">surprise me</button>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
     </article>
   );
 };
